dc8d93f8b6a74a3547ec05e8d0f0d84df158329c
Christopher Sinclair <cmsinc04@cardmail.louisville.edu>
2017-11-08 13:29:14 -0500

    Updated startup.sh

--- startup.sh
+++ startup.sh
@@ -3,9 +3,12 @@
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-triggers
 vagrant up
-vagrant suspend
+vagrant halt
 HDD_UUID=`VBoxManage showvminfo redcap-secure | grep 'SATA.*UUID' | sed 's/^.*UUID: \(.*\))/\1/'`
 USERNAME="redcap_user"
 VBoxManage encryptmedium $HDD_UUID --newpassword - --newpasswordid $USERNAME --cipher "AES-XTS256-PLAIN64"
 echo $USERNAME > .password_id
 vagrant up
+vagrant ssh -c 'sudo mkdir -p /etc/puppet/modules'
+vagrant ssh -c 'sudo puppet module install puppetlabs/stdlib'
+vagrant ssh -c 'sudo puppet apply /vagrant/manifests/manifest.pp'
